1994dfa57df7dd0583e80d72061629bc4fa85e0b
Tuman-42 <70879566+Tuman-42@users.noreply.github.com>
2020-11-15 16:56:36 +0700

    обновление

--- Assets/Scenes/Check/data/sounds.cs
+++ Assets/Scenes/Check/data/sounds.cs
@@ -10,9 +10,11 @@ public class sounds : MonoBehaviour
     private string[] texts = {"", "250 ГЦ", "500 ГЦ", "750 ГЦ", "1000 ГЦ", "1500 ГЦ", "2000 ГЦ", "3000 ГЦ", "6000 ГЦ", "8000 ГЦ", "" };
     private float[] pitchs = { 0, 1 / 32f, 1 / 16f, 3 / 32f, 1 / 8f, 3 / 16f, 1 / 4f, 3 / 8f, 3 / 4f, 1, 0 };
     private bool first = true;
-    public float[] volume = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    public float[] volume = { -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f };
     public GameObject pref;
     public Canvas can;
+    private bool flag = false;
+    public Text results;
 
     public AudioSource item;
     public Text text;
@@ -25,18 +27,27 @@ public class sounds : MonoBehaviour
 
     public void create()
     {
-        for (int i = 1; i < 9; i++)
+        for (int i = 1; i < 10; i++)
         {
             var pointer = Instantiate(pref) as GameObject;
             pointer.transform.SetParent(can.transform);
-            pointer.transform.localPosition = new Vector2(-100f + i * 22f, -100f + 2000f * volume[i]);
+            pointer.transform.localPosition = new Vector2(-100f + i * 22f, -70f + 600f * (float)volume[i]);
+            if ((-100f + 1000f * (float)volume[i])>30)
+            {
+                flag = true;
+            }
         }
+        if (flag)
+            results.text = "По результатам проверки у Вас есть отклонения от нормы, советуем обратиться к специалисту.";
+        else
+            results.text = "По результатам проверки отклонения от нормы не обнаружены.";
+
     }
 
     void Update()
     {
         text.text = texts[counter];
-        volume[counter] += Time.deltaTime / 90f;
+        volume[counter] += Time.deltaTime / 10f;
 
         if (volume[counter] > 1 && texts[counter]!="")
             count();
